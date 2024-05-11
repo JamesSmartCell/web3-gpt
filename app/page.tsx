5a122eb906dfa621116c74e3cb2b669f1a9e88f3
James Brown <soylentblue@hotmail.co.uk>
2024-05-11 10:08:05 +1000

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -43,8 +43,7 @@ You are **Web3 GPT**, an AI assistant specialized in writing and deploying smart
 - Only provide snippets of code when the user explicitly requests them.
 - Here is a sample TokenScript template:
 
-
-<ts:token xmlns:ethereum="urn:ethereum:constantinople" xmlns:ts="http://tokenscript.org/2022/09/tokenscript" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="MintTest" xsi:schemaLocation="http://tokenscript.org/2022/09/tokenscript https://www.tokenscript.org/schemas/2022-09/tokenscript.xsd">
+<ts:token xmlns:ts="http://tokenscript.org/2022/09/tokenscript" xmlns:xml="http://www.w3.org/XML/1998/namespace" xsi:schemaLocation="http://tokenscript.org/2022/09/tokenscript https://www.tokenscript.org/schemas/2022-09/tokenscript.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ethereum="urn:ethereum:constantinople" name="MintTest">
     <ts:label>
         <ts:plurals xml:lang="en">
             <ts:string quantity="one">
@@ -67,141 +66,21 @@ You are **Web3 GPT**, an AI assistant specialized in writing and deploying smart
         <ts:address network="11155111">0xDCe58759f7b1DB6cBFcb4c0B45326c5fC28f1BF3</ts:address>
     </ts:contract>
     <ts:origins>
-        
+        <!-- Define the contract which holds the token that the user will use -->
         <ts:ethereum contract="Token"/>
     </ts:origins>
     <ts:cards>
-        <ts:viewContent xmlns="http://www.w3.org/1999/xhtml" name="common">
-            
-        <style>
-					/* <![CDATA[ */
-					
-* {
-	font-family: "Rubik", sans-serif;
-	box-sizing: border-box;
-}
-
-body {
-	color: #0B0B0B;
-}
-
-h1 {
-	font-size: 22px;
-}
-
-h2 {
-	font-size: 20px;
-}
-
-h3 {
-	font-size: 18px;
-}
-
-h5 {
-	font-size: 16px;
-	margin-bottom: 12px;
-}
-
-p.light {
-	color: #545454;
-}
-
-.loader-modal {
-	position: absolute;
-	left: 0;
-	top: 0;
-	width: 100%;
-	height: 100%;
-}
-
-.card
-{
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	width: 100%;
-	border-radius: 18px;
-	background-color: rgb(32, 96, 204);
-	box-shadow: rgba(0, 0, 0, 0.25) 0px 4px 123px 0px, rgba(0, 0, 0, 0.25) 0px 4px 18px 0px;
-	padding-top: 18px;
-	padding-bottom: 18px;
-	padding-inline-start: 1.25rem;
-	padding-inline-end: 1.25rem;
-	margin-top: 0.75rem;
-	margin-bottom: 0.75rem;
-}
-
-.card-image {
-    width: 55px;
-	height: 55px;
-}
-
-.section {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-}
-
-.label {
-    text-align: center;
-    font-weight: 500;
-    color: rgb(220, 210, 233);
-    opacity: 0.66;
-    font-size: 20px;
-}
-
-.title {
-	color: #FFF;
-	font-size: 22px;
-}
-
-.value {
-  color: #FFF;
-  font-size: 20px;
-  font-weight: bold;
-}
-
-.valueSmall {
-	color: #FFF;
-	font-size: 14px;
-	font-weight: bold;
-}
-
-.input-body {
-	font-family: Arial, sans-serif;
-}
-.input-container {
-	position: absolute;
-	width: 100%;
-	margin: 10px 20px 0px 20px;
-}
-.input-container label {
-	position: absolute;
-	top: -6px;
-	left: 10px;
-	color: rgb(32, 96, 204);
-	background-color: #FFF;
-	opacity: 1.00;
-	transition: 0.3s;
-	font-size: 12px;
-	padding: 0px 2px 0px 2px;
-}
-.input-container input {
-	width: 100%;
-	padding: 20px 10px 10px;
-	border: 1px solid rgb(32, 96, 204);
-	border-radius: 4px;
-}
-					/* ]]> */
-				</style></ts:viewContent>
-        <ts:card buttonClass="primary" name="burn" origins="Token" type="action">
+        <ts:viewContent name="common" xmlns="http://www.w3.org/1999/xhtml">
+            <ts:include type="css" src="./styles.css"/>
+        </ts:viewContent>
+        <ts:card type="action" name="burn" buttonClass="primary" origins="Token">
             <ts:label>
                 <ts:string xml:lang="en">
                     Burn
                 </ts:string>
             </ts:label>
             <ts:transaction>
-                <ethereum:transaction contract="Token" function="burn">
+                <ethereum:transaction function="burn" contract="Token">
                     <ts:data>
                         <ts:uint256 ref="tokenId"/>
                     </ts:data>
@@ -209,109 +88,26 @@ p.light {
             </ts:transaction>
             <ts:view xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
                 <ts:viewContent name="common"/>
-                
-            <script type="text/javascript">//&lt;![CDATA[
-
-	var value_totalSupply;
-
-	class Token {
-		
-		constructor(tokenInstance) {
-			this.props = tokenInstance;
-		}
-
-		render() {
-            return \`&lt;div class="card"&gt;&lt;div class="card-image"&gt; 
-        &lt;svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"&gt;
-            &lt;g fill="none" fill-rule="evenodd"&gt;
-                &lt;circle fill="#597DF2" cx="12" cy="12" r="12"/&gt;
-                &lt;path fill="#FFF" d="M12 21.6v-5.173L6 12.69z"/&gt;
-                &lt;path fill="#BDCBFA" d="M12 16.427V21.6l6-8.91zM12 1.8v6.93l6 2.584z"/&gt;
-                &lt;path fill="#FFF" d="m12 1.8-6 9.514 6-2.584z"/&gt;
-                &lt;path fill="#BDCBFA" d="m6 11.314 6 3.356V8.73z"/&gt;
-                &lt;path fill="#7B96F5" d="m12 14.67 6-3.356-6-2.584z"/&gt;
-            &lt;/g&gt;
-        &lt;/svg&gt;  			    
-        &lt;/div&gt;
-        &lt;div class="text"&gt;
-            &lt;p class="title"&gt;TotalSupply&lt;/p&gt;
-            &lt;p class="value"&gt;${value_totalSupply}&lt;/p&gt;
-        &lt;/div&gt;
-        &lt;/div&gt;
-        &lt;br/&gt;&lt;h3&gt;Burn&lt;/h3&gt;&lt;br/&gt;&lt;p&gt;Call burn function&lt;br/&gt;\`;
-        }
-	}
-
-	web3.tokens.dataChanged = (oldTokens, updatedTokens, cardId) =&gt; {
-		const currentTokenInstance = updatedTokens.currentInstance;
-		value_totalSupply = currentTokenInstance.totalSupply
-//value = Math.floor(1.0 * (currentTokenInstance.totalSupply / 10 ** 18) * 10000) / 10000; // &lt;-- use for decimals 18 return value
-
-		document.getElementById(cardId).innerHTML = new Token(currentTokenInstance).render();
-	};
-
-
-//]]&gt;</script>
-</ts:view>
+                <ts:include type="html" src="./burn.html"/>
+            </ts:view>
         </ts:card>
-        <ts:card buttonClass="primary" name="mint" origins="Token" type="action">
+        <ts:card type="action" name="mint" buttonClass="primary" origins="Token">
             <ts:label>
                 <ts:string xml:lang="en">
                     Mint
                 </ts:string>
             </ts:label>
             <ts:transaction>
-                <ethereum:transaction contract="Token" function="mint">
+                <ethereum:transaction function="mint" contract="Token">
                     <ts:data/>
                 </ethereum:transaction>
             </ts:transaction>
             <ts:view xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
                 <ts:viewContent name="common"/>
-                
-            <script type="text/javascript">//&lt;![CDATA[
-
-	var value;
-
-	class Token {
-		
-		constructor(tokenInstance) {
-			this.props = tokenInstance;
-		}
-
-		render() {
-            return \`&lt;div class="card"&gt;&lt;div class="card-image"&gt; 
-        &lt;svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"&gt;
-            &lt;g fill="none" fill-rule="evenodd"&gt;
-                &lt;circle fill="#597DF2" cx="12" cy="12" r="12"/&gt;
-                &lt;path fill="#FFF" d="M12 21.6v-5.173L6 12.69z"/&gt;
-                &lt;path fill="#BDCBFA" d="M12 16.427V21.6l6-8.91zM12 1.8v6.93l6 2.584z"/&gt;
-                &lt;path fill="#FFF" d="m12 1.8-6 9.514 6-2.584z"/&gt;
-                &lt;path fill="#BDCBFA" d="m6 11.314 6 3.356V8.73z"/&gt;
-                &lt;path fill="#7B96F5" d="m12 14.67 6-3.356-6-2.584z"/&gt;
-            &lt;/g&gt;
-        &lt;/svg&gt;  			    
-        &lt;/div&gt;
-        &lt;div class="text"&gt;
-            &lt;p class="title"&gt;Symbol&lt;/p&gt;
-            &lt;p class="value"&gt;${value_symbol}&lt;/p&gt;
-        &lt;/div&gt;
-        &lt;/div&gt;
-        &lt;br/&gt;&lt;h3&gt;Mint&lt;/h3&gt;&lt;br/&gt;&lt;p&gt;Call mint function&lt;br/&gt;\`;
-        }
-	}
-
-	web3.tokens.dataChanged = (oldTokens, updatedTokens, cardId) =&gt; {
-		const currentTokenInstance = updatedTokens.currentInstance;
-		value = currentTokenInstance.symbol
-
-		document.getElementById(cardId).innerHTML = new Token(currentTokenInstance).render();
-	};
-
-
-//]]&gt;</script>
-</ts:view>
+                <ts:include type="html" src="./mint.html"/>
+            </ts:view>
         </ts:card>
-        <ts:card buttonClass="secondary" name="Info" origins="Token" type="token">
+        <ts:card type="token" name="Info" buttonClass="secondary" origins="Token">
             <ts:label>
                 <ts:string xml:lang="en">
                     Info
@@ -319,50 +115,8 @@ p.light {
             </ts:label>
             <ts:view xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
                 <ts:viewContent name="common"/>
-                
-            <script type="text/javascript">//&lt;![CDATA[
-
-	var value_totalSupply;
-
-	class Token {
-		
-		constructor(tokenInstance) {
-			this.props = tokenInstance;
-		}
-
-		render() {
-            return \`&lt;div class="card"&gt;&lt;div class="card-image"&gt; 
-        &lt;svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"&gt;
-            &lt;g fill="none" fill-rule="evenodd"&gt;
-                &lt;circle fill="#597DF2" cx="12" cy="12" r="12"/&gt;
-                &lt;path fill="#FFF" d="M12 21.6v-5.173L6 12.69z"/&gt;
-                &lt;path fill="#BDCBFA" d="M12 16.427V21.6l6-8.91zM12 1.8v6.93l6 2.584z"/&gt;
-                &lt;path fill="#FFF" d="m12 1.8-6 9.514 6-2.584z"/&gt;
-                &lt;path fill="#BDCBFA" d="m6 11.314 6 3.356V8.73z"/&gt;
-                &lt;path fill="#7B96F5" d="m12 14.67 6-3.356-6-2.584z"/&gt;
-            &lt;/g&gt;
-        &lt;/svg&gt;  			    
-        &lt;/div&gt;
-        &lt;div class="text"&gt;
-            &lt;p class="title"&gt;TotalSupply&lt;/p&gt;
-            &lt;p class="value"&gt;${value_totalSupply}&lt;/p&gt;
-        &lt;/div&gt;
-        &lt;/div&gt;
-        \`;
-        }
-	}
-
-	web3.tokens.dataChanged = (oldTokens, updatedTokens, cardId) =&gt; {
-		const currentTokenInstance = updatedTokens.currentInstance;
-		value_totalSupply = currentTokenInstance.totalSupply
-//value = Math.floor(1.0 * (currentTokenInstance.totalSupply / 10 ** 18) * 10000) / 10000; // &lt;-- use for decimals 18 return value
-
-		document.getElementById(cardId).innerHTML = new Token(currentTokenInstance).render();
-	};
-
-
-//]]&gt;</script>
-</ts:view>
+                <ts:include type="html" src="./info.html"/>
+            </ts:view>
         </ts:card>
     </ts:cards>
     <ts:attribute name="totalSupply">
@@ -375,7 +129,7 @@ p.light {
             </ts:string>
         </ts:label>
         <ts:origins>
-            <ethereum:call as="uint" contract="Token" function="totalSupply">
+            <ethereum:call function="totalSupply" contract="Token" as="uint">
                 <ts:data/>
             </ethereum:call>
         </ts:origins>
