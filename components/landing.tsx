49e75eb9a11d35798645760675d4a63fa905ddb1
James Brown <soylentblue@hotmail.co.uk>
2024-05-11 10:27:35 +1000

    Add TokenScript text

--- components/landing.tsx
+++ components/landing.tsx
@@ -58,7 +58,7 @@ export function Landing({ disableAnimations = false }) {
             sizes="(max-width: 318px) 100vw, 318px"
           />
         </div>
-        <p className="text-lg font-bold tracking-tight lg:text-2xl lg:font-normal">Deploy smart contracts with AI</p>
+        <p className="text-lg font-bold tracking-tight lg:text-2xl lg:font-normal">Deploy smart contracts and TokenScripts with AI</p>
 
         <div className="grid-row-3 my-5 mb-8 grid grid-flow-row gap-1 md:grid-flow-col md:gap-4">
           <div className="mx-3 grid grid-cols-3 content-center gap-1 md:grid-cols-1 md:gap-4">
