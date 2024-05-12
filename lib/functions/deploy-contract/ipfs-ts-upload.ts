73d68c5f5a8a0624201cc415af24c5f6593e0ce6
James Brown <soylentblue@hotmail.co.uk>
2024-05-12 13:58:32 +1000

    update route

--- lib/functions/deploy-contract/ipfs-ts-upload.ts
+++ lib/functions/deploy-contract/ipfs-ts-upload.ts
@@ -1,7 +1,6 @@
 import { NFTStorage } from "nft.storage"
 
 const NFT_STORAGE_TOKEN = process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY || ""
-console.log("NFT_STORAGE_API_KEY", NFT_STORAGE_TOKEN)
 const client = new NFTStorage({ token: NFT_STORAGE_TOKEN })
 
 const ipfsStoreFile = async (content: string): Promise<string> => {
